ff34ef427056225ed21b86646d100e125e340e6c
Helen <kirren@live.com>
2017-06-20 16:54:29 +0300

    Add files via upload

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+// Third Party
+
+//= ../../bower_components/tether/dist/js/tether.min.js
+//= ../../bower_components/jquery/dist/jquery.min.js
+//= ../../bower_components/bootstrap/dist/js/bootstrap.min.js
+
+
+// Custom JS
+
+$( document ).ready( function(){} );
+
